29331d5aabca8c5a62a32a702ab75f1956d50d4e
Daniel <85037387+villadv@users.noreply.github.com>
2022-01-01 22:44:14 -0800

    learning about react hooks useState and useEffect

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { StyledGame, StyledScore, StyleTimer, StyledCharacter } from '../styled/Game';
 import { Strong } from '../styled/Random';
 
+
 export default function Game(){
+    const [score, setScore] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setScore((prevScore) => prevScore + 1);
+            }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+            }, [score]);
+    
+ 
+
     return (
         <StyledGame>
-            <StyledScore>Score:0</StyledScore>
+            <StyledScore>
+                Score:<Strong>{score}</Strong>
+            </StyledScore>
             <StyledCharacter>A</StyledCharacter>
             <StyleTimer>Time: <Strong>00: 0000</Strong></StyleTimer>
         </StyledGame>
